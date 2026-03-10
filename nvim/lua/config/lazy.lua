b9e4da0bae98771d543961fa1b6a690db80821bc
Carlos <41452910+usarral@users.noreply.github.com>
2026-03-10 21:42:12 +0100

    fix: remove git filter from lazy.nvim for better plugin compatibility

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -2,7 +2,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
         local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+        local out = vim.fn.system({ "git", "clone", "--branch=stable", lazyrepo, lazypath })
         if vim.v.shell_error ~= 0 then
                 vim.api.nvim_echo({
                         { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -29,7 +29,7 @@ require("lazy").setup({
         },
         -- Configure any other settings here. See the documentation for more details.
         -- colorscheme that will be used when installing plugins.
-        install = { defaults = { git = { filter = "blob:none" } }, colorscheme = { "habamax" } },
+        install = { colorscheme = { "habamax" } },
         -- automatically check for plugin updates
         checker = { enabled = false },
         performance = {
